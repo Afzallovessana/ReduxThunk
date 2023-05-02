414a91df3aa7870ba951977538d0f44e6d1791e4
Afzallovessana <125810400+Afzallovessana@users.noreply.github.com>
2023-05-02 18:35:26 +0530

    Redux Thunk

---
+++ src/components/IcecreamContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IceCreamContainer = () => {
+  return (
+    <div>
+      <h1>Hello From IceCreamContainer</h1>
+    </div>
+  )
+}
+
+export default IceCreamContainer
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Afzal = () => {
+  return (
+    <div>
+      <h1>Hello From user Container</h1>
+    </div>
+  )
+}
+
+export default Afzal
